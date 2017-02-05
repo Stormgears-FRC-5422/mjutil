36392788fe0926582260bb8dc510bb69f65068da
W. Mark Smith <wmsmith@gmail.com>
2017-02-05 19:40:37 +0000

    small changes to mjifile

--- mjcommon/mjifile.cpp
+++ mjcommon/mjifile.cpp
@@ -1,1 +1,32 @@
+#include <cstdio>
+#include <cstdint>
+#include <cstring>
+
+#include <fcntl.h>
+#include <unistd.h>
+
 #include "mjifile.h"
+
+
+const uint16_t V_MAJ = 1;
+const uint16_t V_MIN = 0;
+ const uint16_t ENDIAN_MAGIC = 0x1234;
+const char *MjiFile::HDR_MAGIC = "MJI File";
+
+MjiFile::MjiFile() {
+    fd = -1;
+}
+
+MjiFile::~MjiFile() {
+    if (fd > 0) close(fd);
+}
+
+void MjiFile::WriteHeader() {
+    header_t hdr;
+
+    memset((char *)&hdr, 0, sizeof(hdr));
+    snprintf(hdr.desc, sizeof(hdr.desc), "%s", HDR_MAGIC);
+    hdr.endian = ENDIAN_MAGIC;
+    hdr.v_maj = V_MAJ;
+    hdr.v_min = V_MIN;
+}
--- mjcommon/mjifile.h
+++ mjcommon/mjifile.h
@@ -3,10 +3,25 @@
 
 class MjiFile {
 public:
+    static const uint16_t V_MAJ;
+    static const uint16_t V_MIN;
+    static const uint16_t ENDIAN_MAGIC;
+    static const char *HDR_MAGIC;
+
     MjiFile();
+    ~MjiFile();
+    bool OpenPcap(const char *fname);
+    bool OpenMji(const char *fname, bool rdonly = true);
 
 private:
     int fd;
+
+    typedef struct {
+        char desc[16];
+        uint32_t endian;
+        uint16_t v_maj;
+        uint16_t v_min;
+    } header_t;
 
     void ReadHeader();
     void WriteHeader();
