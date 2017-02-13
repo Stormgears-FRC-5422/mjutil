54cd371f9baa3dbab68ef19acdb41402aebd2a91
W. Mark Smith <wmsmith@gmail.com>
2017-02-12 22:55:04 -0500

    made generic debian build script

---
+++ package/mk_deb.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+PKG=mjutil
+
+WD=$(dirname $0)
+. /etc/os-release
+dist=$ID$VERSION_ID
+
+gitdescr=$(git describe)
+gitversion=$(cut -f1 -d';' <<< ${gitdescr/-/;})
+gitrelease=$(cut -f2 -d';' <<< ${gitdescr/-/;})
+gitrelease=${gitrelease/-/.}.$dist
+gitlog=$(git log -n 1 --oneline)
+gitname=$(git config --get user.name)
+gitemail=$(git config --get user.email)
+gitdate=$(date -R)
+
+if [ "x${gitrelease}" == "x" ]; then
+  gitrelease=1
+fi
+
+# copy source tree with version in directory name for deb build
+rm -rf $WD/../../${PKG}-*
+mkdir -p $WD/../../${PKG}-${gitversion}/debian
+rsync -aP $WD/../../${PKG}/ $WD/../../${PKG}-${gitversion}/
+
+cat > $WD/../../${PKG}-${gitversion}/debian/changelog <<EOF
+$PKG (${gitversion}-${gitrelease}) unstable; urgency=low
+
+  * ${gitlog}
+
+ -- ${gitname} <${gitemail}>  ${gitdate}
+EOF
+
+cp -r $WD/deb/*.ex $WD/deb/{compat,control,copyright,debhelper-build-stamp,docs,rules,source} $WD/../../${PKG}-${gitversion}/debian
+
+cd $WD/../../${PKG}-${gitversion}
+dpkg-buildpackage -us -uc -b
+
